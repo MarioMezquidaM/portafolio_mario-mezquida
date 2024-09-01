6ad41bf1ac028631b28553843175da74eb7fde97
Mario Mezquida M <65036484+MarioMezquidaM@users.noreply.github.com>
2024-09-01 11:21:00 -0500

    Delete portafolio_mario directory
    carpeta completa

--- portafolio_mario/portafolio_mario/views/about.py
+++
@@ -1,9 +0,0 @@
-import reflex as rx
-from portafolio_mario.components.heading import heading
-
-
-def about(description: str) -> rx.Component:
-    return rx.vstack(
-        heading("Sobre mí"),
-        rx.text(description)
-    )
